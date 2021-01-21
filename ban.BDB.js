684308872653e35bb04ba369d58a554ccb83c4e2
GamesOfficial <74829915+GamesOfficial@users.noreply.github.com>
2021-01-21 10:20:22 -0500

    Create ban.BDB.js

---
+++ ban.BDB.js
@@ -0,0 +1,13 @@
+$ban
+$argsCheck[>1;❎ | Please mention somebody to ban]
+$onlyIf[$mentioned[<]==$authorID;❎ | you can't ban yourself]
+$onlyPerms[ban;❎ | your missing ban members permission]
+$onlyBotPerms[ban;❎ | I am missing ban members perm]
+$title[Ban successfull]
+$description[
+ban ID: $messageID
+
+banned: <@$mentioned[1]>
+
+reason: $noMentionMessage]
+$footer[banned by $username]
