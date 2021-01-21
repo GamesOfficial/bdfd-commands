7bd6591f1c15a4a723d3097181fcbd1d04a46014
GamesOfficial <74829915+GamesOfficial@users.noreply.github.com>
2021-01-21 10:14:34 -0500

    BDB.js
    This coding is bdfd, bdb may use the same language but it's different

---
+++ BDB.js
@@ -0,0 +1,8 @@
+$onlyIf[$message<100;❎ | You can't delete more than 100 messages]
+$onlyBotPerms[managemessages;❎ | I am missing manage messages perm]
+$onlyPerms[managemessages;❎ | you are missing manage messages perm]
+✅ You have successfully cleared $message messages
+$supressErrors[There was an Error trying to delete the messages]
+$deletecommand
+$deleteIn[3s]
+$clear[$message]
